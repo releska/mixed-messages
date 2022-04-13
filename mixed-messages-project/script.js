faae3bccfdbd42c714a4022e70fc7cd97ec5a1ce
alimalim77 <52186295+alimalim77@users.noreply.github.com>
2022-04-13 09:37:49 +0530

    Quotes Check

--- mixed-messages-project/script.js
+++ mixed-messages-project/script.js
@@ -2,16 +2,16 @@
 //test format for quotes & other variables
 const quotes = [
     {
-    author: 'author 1',
-    quote: 'quote 1'      
+    author: 'Winston Churchill',
+    quote: '"Success is not final, failure is not fatal: it is the courage to continue that counts."'      
     },
     {
-    author: 'author 2',
-    quote:'quote 2'
+    author: '“You define your own life. Don’t let other people write your script.”',
+    quote:'Oprah Winfrey'
     },
     {
-    author: 'author 3',
-    quote: 'quote 3'
+    author: '“Nothing in life is to be feared, it is only to be understood. Now is the time to understand more, so that we may fear less.”',
+    quote: 'Marie Curie'
     }
 ]
 let currentAuthor = document.querySelector('.author-paragraph')
