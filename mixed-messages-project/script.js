cfc3a7a4b712da3774967fac10f6f2c8255c2c54
Sumaine210 <jordanderby18@gmail.com>
2022-05-01 00:45:14 -0500

    style overhaul and bug fixes

--- mixed-messages-project/script.js
+++ mixed-messages-project/script.js
@@ -1,44 +1,36 @@
 //test format for quotes & other variables
 const quotes = [
     {
-    author: '-Winston Churchill',
-    quote: '“Success is not final, failure is not fatal: it is the courage to continue that counts”'      
+    author: 'Winston Churchill',
+    quote: 'Success is not final, failure is not fatal: it is the courage to continue that counts'      
     },
     {
-    quote: '“You define your own life. Don’t let other people write your script.”',
-    author:'-Oprah Winfrey'
-    },
-    {
-    quote: '“Out of the mountain of despair, a stone of hope”',
-    author: '-Martin Luther King Jr.'
-    },
-    {
-        quote: '“I am like a clean wall, you are a picture and a frame. I am an empty silence, you are the most beautiful music I know”',
-        author: '-Miladin Šobić'
-    },
-    {
-    quote: '“The crowd has no judgment”',
-    author: '-Sula Radov'
-    },
-    {
-=======
-    },
-    {
-    quote: '“You define your own life. Don’t let other people write your script.”',
-
+    quote: 'You define your own life. Don’t let other people write your script.',
     author:'Oprah Winfrey'
     },
     {
-    quote: '“Nothing in life is to be feared, it is only to be understood. Now is the time to understand more, so that we may fear less.”',
+    quote: 'Out of the mountain of despair, a stone of hope',
+    author: 'Martin Luther King Jr.'
+    },
+    {
+    quote: 'I am like a clean wall, you are a picture and a frame. I am an empty silence, you are the most beautiful music I know',
+    author: 'Miladin Šobić'
+    },
+    {
+    quote: 'The crowd has no judgment',
+    author: 'Sula Radov'
+    },
+    {
+    quote: 'You define your own life. Don’t let other people write your script.',
+    author:'Oprah Winfrey'
+    },
+    {
+    quote: 'Nothing in life is to be feared, it is only to be understood. Now is the time to understand more, so that we may fear less.',
     author: 'Marie Curie'
     },
     {
-        quote: 'I am like a clean wall, you are a picture and a frame. I am an empty silence, you are the most beautiful music I know.',
-        author: 'Miladin Sobic'
-    },
-    {
-    quote: 'The crowd has no judgment.',
-    author: 'Sula Radov'
+    quote: 'I am like a clean wall, you are a picture and a frame. I am an empty silence, you are the most beautiful music I know.',
+    author: 'Miladin Sobic'
     },
     {
     quote: 'Heroism is when I defend myself from another, gallantry is when I defend another from myself.',
@@ -46,33 +38,31 @@ const quotes = [
     },
     {
     author: 'Oprah Winfrey',
-    quote: '“You define your own life. Don’t let other people write your script.”
-    author:'-Oprah Winfrey'
-
+    quote: 'You define your own life. Don’t let other people write your script.',
     },
     {
-    quote: '“Out of the mountain of despair, a stone of hope”',
-    author: '-Martin Luther King Jr.'
+    quote: 'Out of the mountain of despair, a stone of hope',
+    author: 'Martin Luther King Jr.'
     },
     {
-        quote: '“I am like a clean wall, you are a picture and a frame. I am an empty silence, you are the most beautiful music I know”',
-        author: '-Miladin Šobić'
+    quote: 'I am like a clean wall, you are a picture and a frame. I am an empty silence, you are the most beautiful music I know',
+    author: 'Miladin Šobić'
     },
     {
-    quote: '“The crowd has no judgment”',
-    author: '-Sula Radov'
+    quote: 'The crowd has no judgment',
+    author: 'Sula Radov'
     },
     {
-    quote: '“Heroism is when I defend myself from another, gallantry is when I defend another from myself”',
-    author: '-Marko Miljanov'
+    quote: "Heroism is when I defend myself from another, gallantry is when I defend another from myself",
+    author: 'Marko Miljanov'
     },
     {
-    author: '-Petar II Petrović Njegoš',
-    quote: '“This world is a tyrant to a tyrant, let alone a kind soul”'
+    author: 'Petar II Petrović Njegoš',
+    quote: "This world is a tyrant to a tyrant, let alone a kind soul"
     },
     {
-    author: '-Marie Curie',
-    quote: '“Nothing in life is to be feared, it is only to be understood. Now is the time to understand more, so that we may fear less”',
+    author: 'Marie Curie',
+    quote: 'Nothing in life is to be feared, it is only to be understood. Now is the time to understand more, so that we may fear less'
     }
 
 ]
@@ -80,28 +70,27 @@ const quotes = [
 const tweet = document.querySelector('.twitter-share-button');
 
 
-let currentAuthor = document.querySelector('.author-paragraph');
-let currentQuote = document.querySelector('.quote-paragraph');
-let button = document.getElementById('button');
-
+let currentAuthor = document.querySelector('.author-target');
+let currentQuote = document.querySelector('.quote-target');
+let generate = document.getElementById('generate-button');
 //funstion to get random quote
 const getRandomQuote = array => array[Math.floor(Math.random() * array.length)];
 
 
 //button to display selected quote
 
-button.addEventListener('click', () => {
+generate.addEventListener('click', () => {
     let oldAuthor = currentAuthor.innerText
     let oldQuote = currentQuote.innerText
     let selectedQuote = getRandomQuote(quotes)
     if (oldQuote === selectedQuote.quote) {
         selectedQuote = getRandomQuote(quotes)
-        currentAuthor.innerText = selectedQuote.author
-        currentQuote.innerText = selectedQuote.quote
-    } else
-    currentAuthor.innerText = selectedQuote.author
-    currentQuote.innerText = selectedQuote.quote
+        currentAuthor.innerText = `-${selectedQuote.author}`
+        currentQuote.innerText = `"${selectedQuote.quote}"`
+    } else 
+    currentAuthor.innerText = `-${selectedQuote.author}`
+    currentQuote.innerText = `"${selectedQuote.quote}"`
     curauth = currentAuthor.innerHTML
     curquote = currentQuote.innerHTML
-    tweet.href = https://twitter.com/intent/tweet?text=${curauth}-${curquote}
+    tweet.href = `https://twitter.com/intent/tweet?text=${curquote}%20${curauth}`
 })
